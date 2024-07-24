0c202e451ded9b41835fcbc9235ead8cabfa96b6
Nikaxe-dev <iamnikaxing@gmail.com>
2024-07-24 20:50:09 +1200

    Add files via upload

---
+++ Scripts/all.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    var link = top.document.createElement("link")
+    link.rel = "icon"
+    link.href = "/images/icon.png"
+    top.document.getElementsByTagName("head")[0].appendChild(link)
+}
