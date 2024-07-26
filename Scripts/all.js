1f8a87746a885084fe4a11d5faec09ab8de66736
Nikaxe-dev <iamnikaxing@gmail.com>
2024-07-27 09:04:18 +1200

    get script from website test

--- Scripts/all.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-    var link = top.document.createElement("link")
-    link.rel = "icon"
-    link.href = "/images/icon.png"
-    top.document.getElementsByTagName("head")[0].appendChild(link)
-}
