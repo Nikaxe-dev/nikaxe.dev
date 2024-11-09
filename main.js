56e96a3d25af92ebada55d07010330a9186dbcff
Nikaxe <iamnikaxing@gmail.com>
2024-11-10 11:46:21 +1300

    Deleting for V3.0

--- main.js
+++
@@ -1,8 +0,0 @@
-const backs = document.getElementsByClassName("back")
-
-for (i = 0; i < backs.length; i++) {
-    const back = backs[i]
-    back.onclick = function () {
-        pages.goback()
-    }
-}
